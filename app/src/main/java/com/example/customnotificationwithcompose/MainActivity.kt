c42715ae55199251985e2d24c6f40fcc645dfbce
krupal <bhuvakrupal01@gmail.com>
2022-07-26 10:40:35 +0530

    ADD : Simple notification

--- app/src/main/java/com/example/customnotificationwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/customnotificationwithcompose/MainActivity.kt
@@ -1,43 +1,71 @@
 package com.example.customnotificationwithcompose
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.customnotificationwithcompose.ui.theme.CustomNotificationWithComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val context = LocalContext.current
+            val channelId = "my channel"
+            LaunchedEffect(key1 = channelId){
+                createNotificationChannel(context, channelId)
+            }
             CustomNotificationWithComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    horizontalAlignment = Alignment.CenterHorizontally ,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    Button(onClick = { simpleNotification(context, channelId)}) {
+                        Text(text = "show simple notification")
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    private fun simpleNotification(context: Context, channelId: String){
+        val notificationBuilder = NotificationCompat.Builder(context,channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("hello world")
+            .setContentText("hello world")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(context)) {
+            notify(0, notificationBuilder.build())
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CustomNotificationWithComposeTheme {
-        Greeting("Android")
+
+    private fun createNotificationChannel(context: Context, channelId : String){
+        val channelName = "my channel"
+        val channelDescription = "my channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val myChannel = NotificationChannel(channelId,channelName,NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = channelDescription
+            }
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(myChannel)
+        }
     }
 }
+
