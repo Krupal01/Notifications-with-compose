6c43d47bf50a2095f02fa21a4dea57146cd72758
krupal <bhuvakrupal01@gmail.com>
2022-07-26 10:43:02 +0530

    ADD : Notification with action

--- app/src/main/java/com/example/customnotificationwithcompose/MainActivity.kt
+++ app/src/main/java/com/example/customnotificationwithcompose/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.customnotificationwithcompose
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -17,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.EXTRA_NOTIFICATION_ID
 import androidx.core.app.NotificationManagerCompat
 import com.example.customnotificationwithcompose.ui.theme.CustomNotificationWithComposeTheme
 
@@ -38,6 +41,10 @@ class MainActivity : ComponentActivity() {
                     Button(onClick = { simpleNotification(context, channelId)}) {
                         Text(text = "show simple notification")
                     }
+
+                    Button(onClick = { actionNotification(context, channelId)}) {
+                        Text(text = "show action notification")
+                    }
                 }
             }
         }
@@ -51,6 +58,26 @@ class MainActivity : ComponentActivity() {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         with(NotificationManagerCompat.from(context)) {
             notify(0, notificationBuilder.build())
+        }
+    }
+
+    private fun actionNotification(context: Context , channelId: String){
+
+        val intent = Intent(context,MyReceiver::class.java).apply{
+            action = "com.notification.dummy"
+            putExtra(EXTRA_NOTIFICATION_ID, 0)
+        }
+        val pendingIntent = PendingIntent.getBroadcast(context,0,intent,0)
+
+        val notificationBuilder = NotificationCompat.Builder(context,channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("hello world")
+            .setContentText("hello world")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(R.drawable.ic_launcher_background,"action",pendingIntent)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(1, notificationBuilder.build())
         }
     }
 
